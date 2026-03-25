4fb0ec4a0afe9d5332fbc4b28bab9dc662234cba
murali-kothuri <murali.kothuri@stu.ocsb.ca>
2026-03-25 13:19:23 -0400

    change and commited

--- main.ts
+++ main.ts
@@ -6,33 +6,33 @@
 */
 
 
-const number1 = randint(0, 99)
-const number2 = randint(0, 99)
+let number1 = randint(0, 99);
+let number2 = randint(0, 99);
 
 // setup
-basic.clearScreen()
-basic.showIcon(IconNames.Happy)
+basic.clearScreen();
+basic.showIcon(IconNames.Happy);
 
 // button A
 input.onButtonPressed(Button.A, function () {
-    basic.showString(" #:" + number1)
-    basic.showIcon(IconNames.Happy)
-})
+    basic.showString(" #:" + number1);
+    basic.showIcon(IconNames.Happy);
+});
 
 // button B
 input.onButtonPressed(Button.B, function () {
-    basic.showString(" #:" + number2)
-    basic.showIcon(IconNames.Happy)
-})
+    basic.showString(" #:" + number2);
+    basic.showIcon(IconNames.Happy);
+});
 
 // shake
 input.onGesture(Gesture.Shake, function () {
     if (number1 < number2) {
-        basic.showString( number1 + "<" + number2)
-        basic.showIcon(IconNames.Sad)
+        basic.showString( number1 + "<" + number2);
+        basic.showIcon(IconNames.Sad);
     } else { 
         (number1 > number2) 
-        basic.showString( number1 + ">" + number2)
-        basic.showIcon(IconNames.Sad)
+        basic.showString( number1 + ">" + number2);
+        basic.showIcon(IconNames.Sad);
     }
-})
+});
